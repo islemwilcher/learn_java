effa60094c2bbc9388878e2cf34ef06a2b84e35c
islemwilcher <islemadobe6@gmail.com>
2021-11-12 01:57:52 +0100

    learnin java for beginners

---
+++ EscapeSequences.java
@@ -0,0 +1,17 @@
+package LEARN_JAVA;
+
+public class EscapeSequences {
+
+	public static void main(String[] args) {
+		//double Quotes
+		System.out.println("my name is \" islem \" and i am a web developer");
+		//typing the backslash 
+		System.out.println("this is a backslash \\");
+		//\t used for extra space
+		System.out.println("we use \t for extra space");
+		//\n used for breaking the line
+		System.out.println("we break the line \n by this  ");
+
+	}
+
+}
---
+++ HelloJava.java
@@ -0,0 +1,17 @@
+package LEARN_JAVA;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		// output the text and break the line
+		System.out.println("( println ): Hello Java");
+		
+		//output the text and continuing on the same line
+		System.out.print("( print ): Hello Java.");
+		
+		// without breaking the line
+		System.out.print("See the difference?");
+	}
+
+}
